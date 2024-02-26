80c26c518504cad4dd119eab9d4469e168f59e1e
Muqeed007 <ahmedalmuqeed2002@gmail.com>
2024-02-26 12:48:17 +0600

    added chain.js

--- chain.js
+++ chain.js
@@ -1,3 +1,36 @@
-function getChain(){
-    console.log();
+const products = {
+    count : 5000,
+    data : [
+        {id:1, name: 'lenove',price: 50000},
+        {id:2, name: 'macbook',price: 150000},
+    ]   
 }
+// access the price of macbook
+console.log(products.data[1].price);
+
+const user1 = {
+    id:5001,
+    name: 'muqeed',
+    address: {
+        street:{
+            first:'64/1',
+            second:'Shah Ali Bag',
+            third:'Mirpur-1',
+        },
+        city: 'Dhaka'
+    }   
+}
+const user2 = {
+    id:5001,
+    name: 'muqeed',
+    address: {
+        street:{
+            first:'64/1',
+            // second:'Shah Ali Bag',
+            third:'Mirpur-1',
+        },
+        city: 'Dhaka'
+    }   
+}
+console.log(user1.address.street?.second);
+console.log(user2.address.street?.second);
